3e025996f280b2152752d3110191bb66f51fe80e
up637415 <up637415@myport.ac.uk>
2015-10-22 17:44:07 +0100

    Insert call buttons for each number.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,54 +11,61 @@ function doClick(e) {
     alert(textField.value.length);
 }
 
-// var phonebox = $.phonesearch;
 textField.addEventListener('change', function(e){
+	
+    // 	Get phoneSearchValue.
 	var phoneSearchValue = textField.value;
-	// var indexValue = indexOf.phoneSearchValue;
-	// alert(indexValue);
+	
+    // 	Check if value is a phone number. 
 	if (phoneSearchValue.length == 11) {
-		// alert(phoneSearchValue);
+
 		var number = phoneSearchValue;
 		
 		// Set JSON URL. 		
 		var url = "http://up637415.co.uk/node.json?field_premium_number=0870 070 0191";
 		var json;
 		
+        // Get contents from URL. 		
 		var xhr = Ti.Network.createHTTPClient({
-			
 			onload: function() {
 			Ti.API.log("JSON:::  " + this.responseText);  
+			
 			// parse the retrieved data, turning it into a JavaScript object
 			var json = JSON.parse(this.responseText);
 			var json_free_phone_numbers = json['list']['0']['field_free_phone'];
 			
-			// var free_phone_fields = JSON.stringify(json['list']['0']['field_free_phone']);
-			
+            // Iterate over each result. Generate button to call. 			
 			json_free_phone_numbers.forEach(function(entry) {
+				
 			    Ti.API.log("Telephone Field:::   " + entry);
+			    var callButton = Titanium.UI.createButton({
+			        top : "300dp",
+			        width : "260dp",
+			        height : "46dp",
+			        title : entry,
+			    });
+			    $.index.add(callButton);
+			    var call = 'tel: ' + entry;
+				var intent = Ti.Android.createIntent({
+				        action : Ti.Android.ACTION_CALL,
+				        data : call
+				        });
+				Ti.Android.currentActivity.startActivity(intent);
 			});
 			
 			
 			}
 		});
 		
+        // Open URL.		
 		xhr.open("GET", url); 
-		
 		xhr.send();
 		
-		// Initiate call.
 		
-		var call = 'tel:' + number;
-		var intent = Ti.Android.createIntent({
-		        action : Ti.Android.ACTION_CALL,
-		        data : call
-		        });
-		// Ti.Android.currentActivity.startActivity(intent);
 	
 	}
         
 });
-
 
 
 $.index.open();
