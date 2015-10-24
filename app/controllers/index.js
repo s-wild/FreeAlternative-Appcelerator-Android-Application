e5768a070d7a9f451ad9aa701b33fb8c59538ac0
up637415 <up637415@myport.ac.uk>
2015-10-24 17:03:02 +0100

    Added a no results if a number is not found. More formating changes as well as an error function if there is a network problem.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,3 @@
-var scrollView = Ti.UI.createScrollView({
-    contentWidth:'auto',
-    contentHeight:'auto',
-    width:350,
-    height:240,
-    top:125,
-    backgroundColor:'red',
-    layout: 'vertical'
-});
-call_buttons = [];
-$.index.add(scrollView);
-
 var textField = Ti.UI.createTextField({
   borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
   color: '#336699',
@@ -19,13 +7,30 @@ var textField = Ti.UI.createTextField({
 });
 $.index.add(textField);
 
+// Hide no result label on load.
+noResults = $.noResults;
+noResults.hide();
+
 textField.addEventListener('change', function(e){
 	
     // 	Get phoneSearchValue.
 	var phoneSearchValue = textField.value;
+	Ti.API.log(phoneSearchValue.length);
 	
     // 	Check if value is a phone number. 
 	if (phoneSearchValue.length == 11) {
+		var scrollView = Ti.UI.createScrollView({
+		    contentWidth:'auto',
+		    contentHeight:'auto',
+		    left: 0, 
+		    right: 0,
+		    height:240,
+		    top:125,
+		    backgroundColor:'transparent',
+		    layout: 'vertical'
+		});
+		call_buttons = [];
+		$.index.add(scrollView);
  
 		var number = phoneSearchValue;
 		
@@ -35,33 +40,50 @@ textField.addEventListener('change', function(e){
 		
         // Get contents from URL. 		
 		var xhr = Ti.Network.createHTTPClient({
+			// function called when the response data is available
 			onload: function() {
 				Ti.API.log("JSON:::  " + this.responseText);  
+				jsonText = this.responseText;
 				
 				// parse the retrieved data, turning it into a JavaScript object
 				var json = JSON.parse(this.responseText);
-				var json_free_phone_numbers = json['list']['0']['field_free_phone'];  
-				
-				Ti.API.log("Field_FREE_PHONE:::  " + json_free_phone_numbers);  
+
+				jsonList = JSON.stringify(json['list']);
+				if (jsonList.length > 2) {
+					
+					Ti.API.log("Somethign is in the array!!!");
+					var json_free_phone_numbers = json['list']['0']['field_free_phone'];  
 	
-	            // Iterate over each result. Generate button to call. 	
-	            var index;
-	            topprop = 0.1; // this is space between two labels one below the other
-				for (index = 0; index < json_free_phone_numbers.length; ++index) {
-					var call_buttons = Titanium.UI.createButton({
-						id: json_free_phone_numbers[index],
-				        top: 15+ index * topprop,
-				        width : "94%",
-				        height : "60dp",
-				        title : json_free_phone_numbers[index],
-				        font: {fontSize: '30'},
-				    });
-	
-				    scrollView.add(call_buttons);
-				    call_buttons.addEventListener('click', callNowButton); 
-				    
-				}	
-			}
+					Ti.API.log("Field_FREE_PHONE:::  " + json_free_phone_numbers);  
+		
+		            // Iterate over each result. Generate button to call. 	
+		            var index;
+		            topprop = 0.1; // this is space between two labels one below the other
+					for (index = 0; index < json_free_phone_numbers.length; ++index) {
+						var call_buttons = Titanium.UI.createButton({
+							id: json_free_phone_numbers[index],
+					        top: 15+ index * topprop,
+					        width : "94%",
+					        height : "60dp",
+					        title : json_free_phone_numbers[index],
+					        font: {fontSize: '30'},
+					    });
+		
+					    scrollView.add(call_buttons);
+					    call_buttons.addEventListener('click', callNowButton); 
+					    
+					}
+				}
+				else {
+					noResults.show(); 
+				}
+					
+			},
+			 // function called when an error occurs, including a timeout
+		     onerror : function() {
+		         Ti.API.log("Error:::  ERRORRR!!!!!!!");
+		     },
+		     timeout : 5000  // in milliseconds
 		});
 		
         // Open URL.		
@@ -70,6 +92,9 @@ textField.addEventListener('change', function(e){
 		
 		
 	
+	}
+	else {
+		noResults.show();
 	}
         
 });
