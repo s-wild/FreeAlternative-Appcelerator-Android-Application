1e8079aa34f2e9ea97bc23324beaa4ce1c720a0e
up637415 <up637415@myport.ac.uk>
2016-01-19 13:26:55 +0000

    Tried adding a delay function on keyup to prevent bugs, does not seem to be working.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -22,12 +22,29 @@ call_buttons = [];
 // $.index.add(activity);
 
 var len=searchInputBox.value.length;
-searchInputBox.addEventListener('change', function(e) {
-	resultsView.removeAllChildren();
 
-  // Get searchInput.
-  var searchInput = searchInputBox.value;
-  
+timer = 0;
+function userSearch (){ 
+    // Get searchInput.
+  	var searchInput = searchInputBox.value;
+  	Ti.API.log("MySearch()");
+	doSearch(searchInput);
+	Ti.API.log("user search");
+
+}
+
+// Set up a listener for on key up.
+searchInputBox.addEventListener('change', function(e) {
+	Ti.API.log("Keyup");
+	resultsView.removeAllChildren();
+	if (timer) {
+        clearTimeout(timer);
+    }
+    timer = setTimeout(userSearch, 500); 
+});
+
+function doSearch (searchInput){ 
+  Ti.API.log("Do Search Started");
   
   // delete results if no input.
   if (searchInput.length == 0) {
@@ -58,7 +75,7 @@ searchInputBox.addEventListener('change', function(e) {
       	Ti.API.log("it's a premium number.");
       	 url = "http://10.0.3.2/fa.dev/httpdocs/views/phone-numbers?field_premium_number_value=" + searchInput;
       	getUrlContents(url);
-      } 
+      }  
     } else {
       Ti.API.log("You have entered a name.");
       // Adjust URL to match name search. 			
@@ -69,10 +86,7 @@ searchInputBox.addEventListener('change', function(e) {
     // Nothing entered, delete everything!
     Ti.API.log("Nothing has been entered, remove everything!");
   }
-
-  
-
-});
+}
 
 // Call now button.
 function callNowButton(id, call_button_number) {
@@ -94,7 +108,7 @@ function IsNumeric(searchInput) {
   return (searchInput - 0) == searchInput && ('' + searchInput).trim().length > 0;
 }
 
-// Function to check if numeric number is in String.
+// Function to check if premium numbers is in String.
 function numberTelephoneCheckPremium(searchInput) {
 	if(searchInput.indexOf('0845') >= 0 || searchInput.indexOf('0870') >= 0 || searchInput.indexOf('0844') >= 0){
 	  return true;
@@ -168,7 +182,7 @@ function getUrlContents(url) {
             },
           });
 
-          scrollView.add(call_buttons);
+          scrollView.add(call_buttons); 
           call_buttons.addEventListener('click', callNowButton);
 
         }
@@ -209,85 +223,3 @@ function createRow(index, resultNodeTitleNoQuotes) {
 }
 
 $.index.open();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    // 	Check if value is a phone number. 
-	if (searchInput.length >= 11) {
-		
-		var number = searchInput;
-		
-		// Set JSON URL. 		 
-		var url = "http://10.0.3.2/fa.dev/httpdocs/views/phone-numbers" + number;
-		var json;
-		
-        // Get contents from URL.
-		var xhr = Ti.Network.createHTTPClient({
-			// function called when the response data is available
-			onload: function() {
-
-				Ti.API.log("JSON:::  " + this.responseText);  
-				jsonText = this.responseText;
-				
-				// parse the retrieved data, turning it into a JavaScript object
-				var json = JSON.parse(this.responseText);
-
-				jsonList = JSON.stringify(json['list']);
-				if (jsonList.length > 2) {
-					
-					Ti.API.log("Somethign is in the array!!!");
-					var json_free_phone_numbers = json['list']['0']['field_free_phone'];  
-	
-					Ti.API.log("Field_FREE_PHONE:::  " + json_free_phone_numbers);  
-		
-		            // Iterate over each result. Generate button to call. 	
-		            var index;
-		            topprop = 0.1; // this is space between two labels one below the other
-					for (index = 0; index < json_free_phone_numbers.length; ++index) {
-						var call_buttons = Titanium.UI.createButton({
-							id: json_free_phone_numbers[index],
-					        top: 15+ index * topprop,
-					        width : "94%",
-					        height : "60dp",
-					        title : json_free_phone_numbers[index],
-					        font: {fontSize: '30'},
-					    });
-		
-					    scrollView.add(call_buttons);
-					    call_buttons.addEventListener('click', callNowButton); 
-					    
-					}
-				}
-				else {
-					noResults.show(); 
-					
-				}
-			},
-			 // function called when an error occurs, including a timeout
-		     onerror : function() {
-		         Ti.API.log("Error:::  ERRORRR!!!!!!!");
-		     },
-		     timeout : 5000  // in milliseconds
-		});
-		
-        // Open URL.		
-		xhr.open("GET", url); 
-		xhr.send();
-	
-	}
-	else {
-		noResults.show();
-	}
-	*/
