da09f9dd39541754d33a2287e30daab5a5e7f1b0
up637415 <up637415@myport.ac.uk>
2016-01-24 19:02:35 +0000

    Added result page.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -136,32 +136,6 @@ function getUrlContents(url) {
 				resultsView.add(row);
 				//resultsView.show();
 			}
-			/*
-			if (jsonList.length > 2) {
-				Ti.API.log("Somethign is in the array!!!");
-				var json_free_phone_numbers = json['list']['0']['field_free_phone'];
-				Ti.API.log("Field_FREE_PHONE:::  " + json_free_phone_numbers);
-				 // Iterate over each result. Generate button to call. 	
-				var index;
-				topprop = 0.1; // this is space between two labels one below the other
-				for (index = 0; index < json_free_phone_numbers.length; ++index) {
-				 	var call_buttons = Titanium.UI.createButton({
-				 		id: json_free_phone_numbers[index],
-				 		top: 15 + index * topprop,
-				 		width: "94%",
-				 		height: "60dp",
-				 		title: json_free_phone_numbers[index],
-				 		font: {
-				 			fontSize: '30'
-				 		},
-			 		});
-			 		scrollView.add(call_buttons);
-			 		call_buttons.addEventListener('click', callNowButton);
-			 	}
-      		} 
-      		else {
-      			noResults.show();
-  			}*/
 	    },
 	    onerror: function() {
 	    	// function called when an error occurs, including a timeout
@@ -190,12 +164,17 @@ function createRow(index, resultNodeTitleNoQuotes) {
     height: '94%'
   });
   row.add(call_buttons);
+  call_buttons.addEventListener('click', callNowButton); 
   return row;
 }
 
 $.index.open();
 
-
+function callNowButton() {
+	Ti.API.log("Call now button clicked.");
+	var win2=Alloy.createController('result').getView(); 
+	win2.open();
+}
 /*
     // 	Check if value is a phone number. 
 	if (searchInput.length >= 11) {
