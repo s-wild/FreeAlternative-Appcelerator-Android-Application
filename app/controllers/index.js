a7aec2371c5fce725de3fc1b53f84bda977d8f0c
up637415 <up637415@myport.ac.uk>
2016-03-12 23:24:30 +0000

    Price functionality implemented. Needs finishing for different numbers.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -57,7 +57,7 @@ var previousSearchLabelText = Ti.UI.createLabel({
 	color:'black',
 	text: 'Search History:',
 	textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
-	top: "15",
+	top: "17",
 	left: "60",
 	width: 230, height: 30,
 	font: { fontSize:20 }
@@ -421,18 +421,47 @@ function numberDetails(id, call_button_number) {
 	
 }
 function getNumberPrice(){
+	
+	// Create Price Window.
+	priceWindow = Ti.UI.createWindow({
+    	fullscreen: false,
+    	backgroundColor: "#fff"
+	});
+	// Create activity loader. 
+	activityIndicatorPricing = Ti.UI.createActivityIndicator({
+		color: '#000',
+		  font: {fontFamily:'Helvetica Neue', fontSize:26, fontWeight:'bold'},
+		  message: 'Loading...',
+		  style: Titanium.UI.ActivityIndicatorStyle.BIG,
+		  top:"40%",
+		  left: "15%",
+		  height:Ti.UI.SIZE,
+		  width:Ti.UI.SIZE
+	});
+	priceWindow.add(activityIndicatorPricing);
+	// eventListeners must always be loaded before the event is likely to fire
+	// hence, the open() method must be positioned before the window is opened
+	priceWindow.addEventListener('open', function (e) {
+	  activityIndicatorPricing.show();
+	  // do some work that takes 6 seconds
+	  // ie. replace the following setTimeout block with your code
+	});
+
+	priceWindow.open({
+	    activityEnterAnimation: Ti.Android.R.anim.fade_in,
+	    activityExitAnimation: Ti.Android.R.anim.fade_out
+	});
 	var currentID = this.id;
 	var idSplitted = currentID.split('|');
 	var number = idSplitted[0];
 	var nid = idSplitted[1];
 	var numberType = getNumberType(number);
+	var numberPrice = getPrice(numberType);
+	
 	Titanium.API.log("numberPrice9999999999" , numberType); 
 	// Set message for feedback dialog popup. Include number to tell user what number they are rating.
 
-	var priceWindow = Ti.UI.createWindow({
-    	fullscreen: false,
-    	backgroundColor: "#fff"
-	});
+	
 	var priceLabel = Ti.UI.createLabel({
 		color:'#000',
 		text: "Pricing Information for " + number,
@@ -442,22 +471,20 @@ function getNumberPrice(){
 		width: "94%",
 		font: { fontSize:23 }
 	});
-	var numberTypeLabel = Ti.UI.createLabel({
-		color:'#000',
-		text: numberType + " Detected... More to come! Need to connect to server with detected value and operator name to get pricing information!",
-		textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
-		top: "80",
-		wordWrap : true, 
-		left: "3%",
-		width: "94%",
-		font: { fontSize:23 }
-	});
+	// var numberTypeLabel = Ti.UI.createLabel({
+		// color:'#000',
+		// //text: numberType + " Detected... More to come! Need to connect to server with detected value and operator name to get pricing information!",
+		// text: "+",
+		// textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+		// top: "80",
+		// wordWrap : true, 
+		// left: "3%",
+		// width: "94%",
+		// font: { fontSize:23 }
+	// });
 	priceWindow.add(priceLabel);
-	priceWindow.add(numberTypeLabel);
-	priceWindow.open({
-	    activityEnterAnimation: Ti.Android.R.anim.fade_in,
-	    activityExitAnimation: Ti.Android.R.anim.fade_out
-	});
+	// priceWindow.add(numberTypeLabel);
+	
 
 	// Add event listener for when submit button is clicked.
 	// numberFeedbackDialog.addEventListener('click', function(e){
@@ -1131,17 +1158,144 @@ function getNumberType(number) {
     switch (true) {
 		case (number.indexOf("0870") > -1):
 			number_type = "0870";
-			return number_type;
 		  break;
 		case (number.indexOf("0800") > -1):
 		    number_type = "0800";
-		    return number_type;
 		  break;
 		case (number.indexOf("0845") > -1):
 		    number_type = "0845";
-		    return number_type;
 		  break;
 	}
+	return number_type;
+}
+// Get Number Price Functionality.
+function getPrice(numberType) {
+	var companyNamesPrice = [];
+	var filtered_results_companies = [];
+	
+
+	if (numberType == "na") {
+		Ti.API.log("::::::::::getPrice", "Number not found.");
+	}
+	else {
+		Ti.API.log("::::::::::getPrice", numberType);
+		var url = "http://up637415.co.uk/telephone-numbers/pricing/" + numberType;
+		 var client = Ti.Network.createHTTPClient({
+		     // function called when the response data is available
+		     onload : function(e) {
+		         Ti.API.info("Received text: " + this.responseText);
+		         //alert('success');
+		        scrollViewPrices = Ti.UI.createScrollView({
+					 showVerticalScrollIndicator: true,
+					 showHorizontalScrollIndicator: true,
+					 top: "20%",
+					 height: '80%',
+					 width: '94%',
+					 backgroundColor: "#EEEEEE"
+				}); 
+		         var json = JSON.parse(this.responseText);
+		         resultNodes = json.prices;
+				//Ti.API.log("resultNodes", JSON.stringify(resultNodes));
+				resultsLength = resultNodes.length;
+				Ti.API.info("resultsLength ", resultsLength);
+				for (index = 0; index < resultsLength; ++index) {
+					var companyName = resultNodes[index].title;
+					var numberType = resultNodes[index].number_type;
+					var accessCharge = resultNodes[index].access_charge;
+					var phonePlan = resultNodes[index].plan;
+					
+					companyNamesPrice.push({company: companyName, number_type: numberType, access_charge: accessCharge, phone_plan: phonePlan});
+				}
+
+				
+	
+				// Group results based on company name.
+				grouped = {};
+				
+				companyNamesPrice.forEach(function (a) {
+				    grouped[a.company] = grouped[a.company] || [];
+				    grouped[a.company].push({ number_type: a.number_type, access_charge: a.access_charge, phone_plan: a.phone_plan });
+				});
+				groupedResults = JSON.stringify(grouped);
+				groupedResultsLength = Object.keys(grouped).length;
+				Ti.API.info("companyNamesPricegrouped ", JSON.stringify(grouped));
+				// Loop over companies in grouped results.
+				for (var key in grouped) {
+					var indexCompany = Object.keys(grouped).indexOf(key);
+					// createPriceView(index, key);
+					var groupKey = grouped[key];
+					
+					var companyPriceLabel = Ti.UI.createLabel({
+					    color: '#000',
+					    text: key,
+					    top: indexCompany*80 + 10,
+					    font: { fontSize:28 }
+					});
+					scrollViewPrices.add(companyPriceLabel);
+					// Loop over details for each company. 
+					for (index = 0; index < groupKey.length; ++index) {
+						Ti.API.info("groupKeyIndex", JSON.stringify(groupKey[index]));
+						var priceValue = groupKey[index].access_charge;
+						// Check if variable is contract or pay as you go.
+						Ti.API.info("companyNamesPricegrouped ", priceValue.length);
+						if (priceValue != 0) {
+							if (groupKey[index].phone_plan == 0 ) {
+								//createPriceEntry(planType, priceValue);
+								var paygValue = Ti.UI.createLabel({
+								    color: '#000',
+								    text: "Contract: £" + priceValue,
+								    top: indexCompany*80+50,
+								    left: "6%",
+								    font: { fontSize:22 }
+								});
+								scrollViewPrices.add(paygValue);
+							}
+							else {
+								Ti.API.info("phone plan is contract");
+								//createPriceEntry(planType, priceValue);
+								var contractValue = Ti.UI.createLabel({
+								    color: '#000',
+								    text: "PAYG: £" + priceValue,
+								    top: indexCompany*80+50,
+								    left: "60%",
+								    font: { fontSize:22 }
+								});
+								scrollViewPrices.add(contractValue);
+							}
+						}
+					}
+				}
+				
+		        // resultsLength = json.length;
+		        // Ti.API.info("groupedResultsLength ", groupedResultsLength);
+		        // Ti.API.info("groupedResults ", groupedResults);
+		        priceWindow.add(scrollViewPrices);
+		        activityIndicatorPricing.hide();
+				
+		     },
+		     // function called when an error occurs, including a timeout
+		     onerror : function(e) {
+		         Ti.API.debug(e.error);
+		         //alert('error');
+		     },
+		     timeout : 5000  // in milliseconds
+		 });
+		 // Prepare the connection.
+		 client.open("GET", url);
+		 // Send the request.
+		 client.send();
+	}
+	
+}
+
+function createPriceEntry(planType, priceValue){
+	if (planType == "payg") {
+		Ti.API.info("phone plan is PAYG");
+	}
+	if (planType == "contract") {
+		Ti.API.info("phone plan is contract");
+	}
+	Ti.API.info("createPriceEntry ");
 }
 // Back button handler.
 $.index.addEventListener('androidback' , function(e){
